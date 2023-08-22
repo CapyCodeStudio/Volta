dbc00f3329f930cc367b06201cd0b4aac1867af6
CapyCodeStudio <capycodestudio@gmail.com>
2023-08-22 09:32:53 -0300

    Organizei todas as pastas principais.

---
+++ Volta GitHub/Assets/- Isa/Scripts 1/Movement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+    private Animator animator;
+    public bool fire3;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Movimentação horizontal e vertical
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Direção de movimento relativa à câmera
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        moveDirection = forward * verticalInput + right * horizontalInput;
+
+        // Pular
+        if (characterController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+                animator.SetBool("Jump", true);
+            }
+            else
+            {
+                animator.SetBool("Jump", false);
+            }
+        }
+
+        // Aplicar gravidade
+        moveDirection.y -= 9.81f * Time.deltaTime;
+
+        // Movimentar o personagem
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            moveSpeed = 10;
+            fire3 = true;
+        }
+        else
+        {
+            moveSpeed = 5;
+            fire3 = false;
+        }
+
+       
+
+    }
+}
+
