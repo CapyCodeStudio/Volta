dcd094a0d5295c2d23824faaacc0c6985e52fee6
CapyCodeStudio <142605621+CapyCodeStudio@users.noreply.github.com>
2023-10-16 22:30:36 -0300

    coisas comentadas

--- Volta GitHub/Assets/==Universo2==/Enemy.cs
+++ Volta GitHub/Assets/==Universo2==/Enemy.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -97,4 +97,4 @@ public class Enemy : MonoBehaviour
             TakeDamage(10);
         }
     }
-}
+}*/
--- Volta GitHub/Assets/==Universo2==/Patrulhar.cs
+++ Volta GitHub/Assets/==Universo2==/Patrulhar.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -41,3 +41,4 @@ public class Patrulhar : MonoBehaviour
         
     }
 }
+*/
