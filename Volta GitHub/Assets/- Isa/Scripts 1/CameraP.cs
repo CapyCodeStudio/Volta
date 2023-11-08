b87d109a61fdac22e410872805ee60d1eacf5f04
CapyCodeStudio <capycodestudio@gmail.com>
2023-11-08 08:22:42 -0300

    Revert "universo1"
    This reverts commit d3dc761e853b8a54590d707bfb3100723c0fbd94.

--- Volta GitHub/Assets/- Isa/Scripts 1/CameraP.cs
+++ Volta GitHub/Assets/- Isa/Scripts 1/CameraP.cs
@@ -15,8 +15,6 @@ public class CameraP : MonoBehaviour
     public GameObject UITUTORIAL1;
     public GameObject VoyagerTerceiraPessoa;
     public GameObject VoyagerPrimeiraPessoa;
-    public GameObject GravadorPrimeira;
-    public GameObject GravadorTerceira; 
 
     private float tempoDeEspera = 2.0f;
 
