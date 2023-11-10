b2510957fb5e5cd61213370efe5dcaeeb2e5cd34
CapyCodeStudio <capycodestudio@gmail.com>
2023-11-10 09:26:28 -0300

    universo 2

--- Volta GitHub/Assets/- Isa/Scripts 1/AudiosPlayer.cs
+++ Volta GitHub/Assets/- Isa/Scripts 1/AudiosPlayer.cs
@@ -7,20 +7,8 @@ public class AudiosPlayer : MonoBehaviour
     [SerializeField] private AudioSource passosAudioSource;
     [SerializeField] private AudioClip[] passosAudioClip;
 
-    [SerializeField] private AudioSource passosRAudioSource;
-    [SerializeField] private AudioClip[] passosRAudioClip;
-
-    [SerializeField] private AudioSource passosLAudioSource;
-    [SerializeField] private AudioClip[] passosLAudioClip;
-
     [SerializeField] private AudioSource correrAudioSource;
     [SerializeField] private AudioClip[] correrAudioClip;
-
-    [SerializeField] private AudioSource correrRAudioSource;
-    [SerializeField] private AudioClip[] correrRAudioClip;
-
-    [SerializeField] private AudioSource correrLAudioSource;
-    [SerializeField] private AudioClip[] correrLAudioClip;
 
     [SerializeField] private AudioSource puloAudioSource;
     [SerializeField] private AudioClip[] puloAudioClip;
@@ -28,28 +16,12 @@ public class AudiosPlayer : MonoBehaviour
     {
         passosAudioSource.PlayOneShot(passosAudioClip[Random.Range(0, passosAudioClip.Length)]);
     }
-    public void PassosR()
-    {
-        passosAudioSource.PlayOneShot(passosAudioClip[Random.Range(0, passosAudioClip.Length)]);
-    }
-    public void PassosL()
-    {
-        passosAudioSource.PlayOneShot(passosAudioClip[Random.Range(0, passosAudioClip.Length)]);
-    }
-
+ 
     public void Correr()
     {
         correrAudioSource.PlayOneShot(correrAudioClip[Random.Range(0, correrAudioClip.Length)]);
     }
-    public void CorrerR()
-    {
-        correrAudioSource.PlayOneShot(correrAudioClip[Random.Range(0, correrAudioClip.Length)]);
-    }
-    public void CorrerL()
-    {
-        correrAudioSource.PlayOneShot(correrAudioClip[Random.Range(0, correrAudioClip.Length)]);
-    }
-
+   
     public void Pulo()
     {
         puloAudioSource.PlayOneShot(correrAudioClip[Random.Range(0, puloAudioClip.Length)]);
