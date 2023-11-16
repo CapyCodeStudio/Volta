82e15f416585c2ce98d4f815836ae776f2fd0c1a
CapyCodeStudio <capycodestudio@gmail.com>
2023-11-16 16:29:57 -0300

    deserto chines e cyberpunk

---
+++ Volta GitHub/Assets/- Isa/Scripts 1/SenhaMorse.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SenhaMorse : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("SenhaMorse");
+        }
+            
+    }
+}
---
+++ Volta GitHub/Assets/==MenuPrincipal==/CameraP2.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraP2 : MonoBehaviour
+{
+    public bool tocando;
+    public CinemachineFreeLook freeLookCamera;
+    public CinemachineVirtualCamera virtualCamera;
+    public GameObject trigger;
+    public GameObject mesh;
+    public GameObject VoyagerTERCEIRA;
+    public GameObject VoyagerPRIMEIRA;
+
+    private float tempoDeEspera = 2.0f;
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            mesh.SetActive(false);
+            tocando = true;
+            freeLookCamera.Priority = 0;
+            virtualCamera.Priority = 5;
+            VoyagerTERCEIRA.SetActive(false);
+            VoyagerPRIMEIRA.SetActive(true);
+ 
+        }
+    }
+    public void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            mesh.SetActive(true);
+            tocando = false;
+            print(tocando);
+            freeLookCamera.Priority = 5;
+            virtualCamera.Priority = 0;
+            VoyagerTERCEIRA.SetActive(true);
+            VoyagerPRIMEIRA.SetActive(false);
+          
+        }
+    }
+}
+
