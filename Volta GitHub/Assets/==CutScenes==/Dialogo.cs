2a5a4e0a53e5ae8aa775faef66a615279c251abb
CapyCodeStudio <142605621+CapyCodeStudio@users.noreply.github.com>
2023-12-01 22:30:41 -0300

    cutscene

--- Volta GitHub/Assets/==CutScenes==/Dialogo.cs
+++ Volta GitHub/Assets/==CutScenes==/Dialogo.cs
@@ -18,13 +18,13 @@ public class Dialogo : MonoBehaviour
         // Defina as frases do di�logo (substitua pelo seu pr�prio di�logo)
         sentences = new string[]
         {
-            "Os holofotes são acesos e revelam a misteriosa figura que ele era. Quero dizer, eu o conhecia mas, não conhecia as suas origens. Independente disso, ele sempre foi bom comigo, era como um pai para mim." 
-            + Environment.NewLine + "Éramos sós, não tínhamos mais ninguém, mas aprendemos a lidar com a solidão de duas pessoas. Tendo aquela vida mediana e pacata, eu jamais imaginaria o que estava por vir."
+            "Os holofotes foram acesos e revelaram a sua misteriosa figura. Eu não conhecia a suas origens, mas agora, sei. Independente disso tudo, ele sempre foi bom comigo, era como um pai para mim." 
+            + Environment.NewLine + "Éramos sós, não tínhamos mais ninguém, e tivemos de aprender a lidar com a solidão de duas pessoas. Tendo aquela vida mediana e pacata, eu jamais imaginaria o que estava por vir."
             + Environment.NewLine + " "
             + Environment.NewLine + "Naquela noite, uma luz intensa e fantasmagórica envolveu meu avô, uma luminosidade tão brilhante que ofuscou tudo ao seu redor. Ele foi levado por aquilo, desaparecendo para sempre."
             + Environment.NewLine + "O seu desaparecimento lançou-me uma sombra de dubiedade existencial, uma sombra da incerteza e da perda em contraste com a luz que o tirou de mim. Isso não é uma quimera; é uma realidade tangível, mas uma realidade que escapa à minha compreensão."
             + Environment.NewLine + " "
-            + Environment.NewLine + "Havia ali nele, uma essência que o fazia seguir sob qualquer circunstância, além de suas virtudes e o seu conhecimento memorável. Esta é a razão pela qual o escolheram, o arrancaram de mim, deixando-me aqui aprisionado, sem respostas."
+            + Environment.NewLine + "Havia ali nele, uma essência que o fazia seguir sob qualquer circunstância, além de suas virtudes e o seu conhecimento memorável. Acredito que esta é a razão pela qual o escolheram, o arrancaram de mim, deixando-me aqui aprisionado, sem respostas."
             
             
         };
